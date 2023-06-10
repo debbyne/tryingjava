c8873a92607dfdef90582cba6cc6f0efe4f461a8
dev <kinyadebbyne@gmail.com>
2023-06-10 17:55:52 +0300

    narrow casting

--- Main.java
+++ Main.java
@@ -5,9 +5,13 @@ public class Main {
       System.out.println(3 + 3);
       String name = "John";
       System.out.println(name);
-      int myInt = 9;
-      double myDouble = myInt;
-      System.out.println(myInt);
-      System.out.println(myDouble);
+      // int myInt = 9;
+      // double myDouble = myInt;
+      // System.out.println(myInt);
+      // System.out.println(myDouble);
+      double myDouble = 9.78d;
+      int myInt = (int) myDouble; // Manual casting: double to int
+      System.out.println(myDouble);   // Outputs 9.78
+      System.out.println(myInt);      // Outputs 9
     }
 }
